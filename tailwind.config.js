fd61ed4b07933448c432a8b1ebb2479a1729c54a
Adrian Deligero <adriandeligero.1030@gmail.com>
2024-06-12 17:01:46 +0800

    First nako- tiwasonon nalng ang sa desktop

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {
+      colors:{
+        'primary':'#D6DFD3',
+        'color2':'#C9CCC6',
+        'navbar':'#555555'
+      }
+    },
+  },
+  plugins: [],
+}
+
